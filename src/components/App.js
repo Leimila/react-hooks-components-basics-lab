8aa040d1d613b7ea502b6de27fde04fe1ffc32f6
Leimila <leimilamitei2016@gmail.com>
2024-11-27 21:56:40 +0300

    components basics lab

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href="#Home">I'm a link!</a>
     </nav>
   );
 }
@@ -16,15 +16,25 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+
+// Define About component
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
+
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
+     
     </div>
   );
 }
